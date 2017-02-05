191f82dfebad24ff065df0bbcf135c9e83f3b2d6
remyavas <remyavas@yahoo.co.in>
2017-02-05 18:32:16 +0530

    hello initial commit 1 master

---
+++ gitinterview/src/test/java/com/Ford/Test/gitinterview/gitprogram.java
@@ -0,0 +1,18 @@
+package com.Ford.Test.gitinterview;
+
+public class gitprogram
+{
+public void hello()
+{
+	
+System.out.println("hello  in  git");
+}
+	public static void main(String[] args)
+	{
+		gitprogram gitpro=new gitprogram();
+		gitpro.hello();
+		
+		
+	}
+
+}
---
+++ gitinterview/src/test/java/com/Ford/Test/gitinterview/trialjava.java
@@ -0,0 +1,16 @@
+package com.Ford.Test.gitinterview;
+
+public class trialjava {
+	public void hello()
+	{
+		
+	System.out.println("hello  in  git");
+	}
+		public static void main(String[] args)
+		{
+			gitprogram gitpro=new gitprogram();
+			gitpro.hello();
+			
+			
+		}
+}
