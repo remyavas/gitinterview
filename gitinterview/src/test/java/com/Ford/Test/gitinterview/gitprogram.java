689f3a3cc44111eb61a5e0f33d7882d86b27158e
remyavas <remyavas@yahoo.co.in>
2017-02-06 06:10:16 +0530

    committing the gitprogram from 2nd one

--- gitinterview/src/test/java/com/Ford/Test/gitinterview/gitprogram.java
+++ gitinterview/src/test/java/com/Ford/Test/gitinterview/gitprogram.java
@@ -9,6 +9,14 @@ System.out.println("hello  in  git");
 System.out.println("how r u");
 
 }
+
+public void helloo()
+{
+	
+System.out.println("helloo  in  git");
+System.out.println("howw r u");
+
+}
 	public static void main(String[] args)
 	{
 		gitprogram gitpro=new gitprogram();
