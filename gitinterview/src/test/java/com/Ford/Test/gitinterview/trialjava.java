cd1ec937d9a51815803c80021c97511ead676693
remyavas <remyavas@yahoo.co.in>
2017-02-07 18:11:13 +0530

    kkkk

--- gitinterview/src/test/java/com/Ford/Test/gitinterview/trialjava.java
+++ gitinterview/src/test/java/com/Ford/Test/gitinterview/trialjava.java
@@ -7,15 +7,16 @@ public class trialjava {
 	System.out.println("hello  in  git");
 	
 	System.out.println("hello  how r u in  git");
+	System.out.println("hello  how r u in  git");
 	}
 		public static void main(String[] args)
 		{
 			gitprogram gitpro=new gitprogram();
-<<<<<<< HEAD
+
 			gitpro.hell();
-=======
-			gitpro.helloo();
->>>>>>> branch 'master' of https://github.com/remyavas/gitinterview
+
+			gitpro.hello1();
+
 			
 			
 		}
