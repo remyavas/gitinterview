851a36b206d53a9bccc99cfc72eded3bdd5886e9
remyavas <remyavas@yahoo.co.in>
2017-03-10 19:06:53 +0530

    dding child to brnch

---
+++ gitinterview/src/test/java/com/Ford/Test/gitinterview/child2.java
@@ -0,0 +1,14 @@
+package com.Ford.Test.gitinterview;
+
+import org.testng.annotations.Test;
+
+public class child2 {
+	@Test
+	public void child()
+	{
+		System.out.println("hello");
+		
+		
+	}
+
+}
